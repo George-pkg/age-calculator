533f26707deecc48e42669f19405727fd9d1f7c6
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-01 12:18:21 -0300

    upgrade

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+
+const DD =  window.document.querySelector('div.Day')
+const MM = window.document.querySelector('div.MM')
+const YY = window.document.querySelector('div.YY')
+
+function bntInput() {
+    alert('teste')
+}
