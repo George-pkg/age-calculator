1846f98479f915c03fdccca3497f7e06251ae5e7
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-06 12:57:11 -0300

    upgrade

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,40 @@
 
-const Current_Date = new Date()
-const DD =  window.document.querySelector('span.DD')
-const MM = window.document.querySelector('span.MM')
-const YY = window.document.querySelector('span.YY')
+let Current_Date = new Date();
+const DD =  window.document.querySelector('span.DD');
+const MM = window.document.querySelector('span.MM');
+const YY = window.document.querySelector('span.YY');
 
 function bntInput() {
-    const Day = Number(window.document.querySelector('#Day').value)
-    const Mouth = Number(window.document.querySelector('#MM').value)
-    const Year = Number(window.document.querySelector('#YY').value)
+    let Day = Number(window.document.querySelector('#Day').value);
+    let Mouth = Number(window.document.querySelector('#MM').value);
+    let Year = Number(window.document.querySelector('#YY').value);
 
-    DD = Current_Date.getFullYear()
+    if (Day <= 31 && Day >= 1) {
+        if(Mouth <= 12 && Mouth >= 1){
+            if(Year <= Current_Date.getUTCFullYear() && Year >= (Current_Date.getUTCFullYear() - 110)) {
+                CalculateDay()
+            }
+        }
+    }else {
+        
+    }
+    
 
     console.log(Day, Mouth, Year);
-    console.log(new Date().getHours());
+    console.log(Current_Date.getUTCDate());
+}
+
+function CalculateDay() {
+    let Day = Number(window.document.querySelector('#Day').value);
+    let Mouth = Number(window.document.querySelector('#MM').value);
+    let Year = Number(window.document.querySelector('#YY').value);
+
+    YY.innerHTML = Current_Date.getUTCFullYear() - Year
+    MM.innerHTML = (Current_Date.getMonth() + 1) - Mouth
+    if (Current_Date.getUTCDate() >= Day){
+        DD.innerHTML = Current_Date.getUTCDate() - Day
+    }else {
+        DD.innerHTML = Day - Current_Date.getUTCDate() 
+    }
+     
 }
