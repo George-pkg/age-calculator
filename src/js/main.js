8c5dcce7673b793b3ba05a5eaf23be88ec677363
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-06 11:20:11 -0300

    upgarde

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 
-
-const DD =  window.document.querySelector('div.Day')
-const MM = window.document.querySelector('div.MM')
-const YY = window.document.querySelector('div.YY')
+const Current_Date = new Date()
+const DD =  window.document.querySelector('span.DD')
+const MM = window.document.querySelector('span.MM')
+const YY = window.document.querySelector('span.YY')
 
 function bntInput() {
-    alert('teste')
+    const Day = Number(window.document.querySelector('#Day').value)
+    const Mouth = Number(window.document.querySelector('#MM').value)
+    const Year = Number(window.document.querySelector('#YY').value)
+
+    DD = Current_Date.getFullYear()
+
+    console.log(Day, Mouth, Year);
+    console.log(new Date().getHours());
 }
